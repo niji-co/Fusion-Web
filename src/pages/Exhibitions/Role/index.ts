527426f9b6cfe7f887fdd7e0cdb9dd7270fc2b0a
Sophie <sophiewongsp@gmail.com>
2021-04-20 14:46:34 +0800

    🔨 REFACTOR (Exhibition): add index.ts content

---
+++ src/pages/Exhibitions/Role/index.ts
@@ -0,0 +1,8 @@
+import Role from "./Role";
+import RoleProps, { RoleTypes } from "./models/RoleProps";
+
+export default Role;
+export {
+  RoleProps,
+  RoleTypes,
+};
---
+++ src/pages/Exhibitions/Section/index.ts
@@ -0,0 +1,7 @@
+import Section from "./Section";
+import SectionProps from "./models/SectionProps";
+
+export default Section;
+export {
+  SectionProps,
+};
