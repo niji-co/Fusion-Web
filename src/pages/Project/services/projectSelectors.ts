4f3f4815d317ff7c40a6eadfa8dbe4996b42bb03
James <jameslimbj@gmail.com>
2021-04-26 23:43:19 +0800

    :sparkles: FEAT: create selector for project row from project id

--- src/pages/Project/services/projectSelectors.ts
+++ src/pages/Project/services/projectSelectors.ts
@@ -1,6 +1,9 @@
-import { RootState } from "../../../services/store";
+import { createSelector, EntityState, OutputSelector } from "@reduxjs/toolkit";
 
+import { RootState } from "../../../services/store";
 import { projectsAdapter } from "./projectsReducer";
+import ProjectModel from "../models/ProjectModel";
+import { ProjectRowProps } from "../Row";
 
 export const {
   selectById: selectProjectById,
@@ -9,3 +12,35 @@ export const {
   selectAll: selectAllProject,
   selectTotal: selectTotalProjects,
 } = projectsAdapter.getSelectors((state: RootState) => state.projects);
+
+export const selectRowsByProjectId = (
+  projectId: string
+): OutputSelector<
+  {
+    projects: EntityState<ProjectModel>;
+    projectRows: EntityState<ProjectRowProps>;
+  },
+  ProjectRowProps[],
+  (
+    res1: ProjectModel | undefined,
+    res2: (ProjectRowProps | undefined)[]
+  ) => ProjectRowProps[]
+> =>
+  createSelector(
+    [
+      (state: RootState) => selectProjectById(state, projectId),
+      (state: RootState) =>
+        state.projectRows.ids.map(id => state.projectRows.entities[id]),
+    ],
+    (project, rows): ProjectRowProps[] => {
+      if (project === undefined) {
+        return [];
+      }
+
+      const filtered: ProjectRowProps[] = rows.filter(
+        row => row !== undefined
+      ) as ProjectRowProps[];
+
+      return filtered.filter(row => project.rows.includes(row.id));
+    }
+  );
