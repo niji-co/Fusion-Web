b675a1b9cf78a8b7dcfc5b8a3796fa98dad69b52
James <jameslimbj@gmail.com>
2021-04-26 23:28:09 +0800

    chore: add project rows to dev server

--- src/dev/server.ts
+++ src/dev/server.ts
@@ -3,10 +3,26 @@ const server = {
     {
       title: "Project A",
       tags: ["Web", "Mobile"],
+      rows: [
+        {
+          id: "12",
+          type: 0,
+          content: "Lorem ipsun",
+          style: 1,
+        },
+      ],
     },
     {
       title: "Project B",
       tags: ["Mobile", "Server"],
+      rows: [
+        {
+          id: "1",
+          type: 0,
+          content: "This is another body",
+          style: 1,
+        },
+      ],
     },
   ],
 };
