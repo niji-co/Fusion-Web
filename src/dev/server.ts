91201851a14c1c8bec6c4af8c221d257c114d81f
James <jameslimbj@gmail.com>
2021-05-17 11:00:26 +0800

    :art: resolve linting complains

--- src/dev/server.ts
+++ src/dev/server.ts
@@ -15,8 +15,7 @@ const server = {
           type: 1,
           asset: {
             name: "image",
-            url:
-              "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1200px-Image_created_with_a_mobile_phone.png",
+            url: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1200px-Image_created_with_a_mobile_phone.png",
           },
         },
       ],
