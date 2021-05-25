90e05b15d64b711cc0d6ab93cd13013a0b0a9063
Sophie <sophiewongsp@gmail.com>
2021-05-25 11:57:46 +0800

    🔨 REFACTOR (exhibition): add model to index file

--- src/pages/Exhibitions/index.ts
+++ src/pages/Exhibitions/index.ts
@@ -1,3 +1,5 @@
 import Exhibitions from "./Exhibitions";
+import ExhibitionModel, { VisibilityTypes } from "./models/ExhibitionModel";
 
 export default Exhibitions;
+export { ExhibitionModel, VisibilityTypes };
