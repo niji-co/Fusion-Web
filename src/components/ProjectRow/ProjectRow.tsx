cb57775d6c35d7217ccdef368a0800ce7e14625a
James <jameslimbj@gmail.com>
2021-06-01 12:05:53 +0800

    :art: FORMAT (project-row): move component to component folder

--- src/components/ProjectRow/ProjectRow.tsx
+++ src/components/ProjectRow/ProjectRow.tsx
@@ -23,17 +23,13 @@ const ProjectRow: React.FC<ProjectRowProps> = ({
 }: ProjectRowProps) => {
   switch (model.type) {
     case ProjectRowType.Text:
-      return <ProjectRowText model={model as ProjectRowTextModel} {...rest} />;
+      return <ProjectRowText model={model} {...rest} />;
     case ProjectRowType.Image:
-      return (
-        <ProjectRowImage model={model as ProjectRowImageModel} {...rest} />
-      );
+      return <ProjectRowImage model={model} {...rest} />;
     case ProjectRowType.Video:
-      return (
-        <ProjectRowVideo model={model as ProjectRowVideoModel} {...rest} />
-      );
+      return <ProjectRowVideo model={model} {...rest} />;
     case ProjectRowType.File:
-      return <ProjectRowFile model={model as ProjectRowFileModel} {...rest} />;
+      return <ProjectRowFile model={model} {...rest} />;
     default:
       return null;
   }
