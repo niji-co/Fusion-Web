b9460bd7d9aad5251cc39224b42ba3b8c75b67be
James <jameslimbj@gmail.com>
2021-06-10 23:16:57 +0800

    :hammer: REFACTOR (model): shift uuid to user

--- src/models/User/Profile.ts
+++ src/models/User/Profile.ts
@@ -1,7 +1,6 @@
 import AssetModel from "models/Asset";
 
 interface ProfileModel {
-  uuid: string;
   profilePicture: AssetModel;
   displayName: string;
   bio: string;
--- src/models/User/index.ts
+++ src/models/User/index.ts
@@ -1,9 +1,12 @@
 import ProfileModel from "./Profile";
+import ProfileQueryModel from "./ProfileQueryModel";
 
 interface UserModel extends ProfileModel {
+  uuid: string;
+  username: string;
   email: string;
   password: string;
 }
 
 export default UserModel;
-export { ProfileModel };
+export { ProfileModel, ProfileQueryModel };
