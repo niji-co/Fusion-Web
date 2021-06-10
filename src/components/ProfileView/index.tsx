33f8d6ff44bbe5715342e06cf1e206004f78b9a3
James <jameslimbj@gmail.com>
2021-06-10 23:23:31 +0800

    :sparkles: FEAT (component): create profile view

---
+++ src/components/ProfileView/index.tsx
@@ -0,0 +1,24 @@
+import React, { HTMLAttributes } from "react";
+import { ProfileModel } from "models/User";
+
+type ProfileProps = ProfileModel & HTMLAttributes<HTMLElement>;
+
+const ProfileView: React.FC<ProfileProps> = ({
+  profilePicture,
+  displayName,
+  bio,
+  contactEmail,
+  contactNumber,
+  className,
+  ...rest
+}: ProfileProps) => (
+  <div className={`profile ${className || ""}`} {...rest}>
+    <img alt={profilePicture.name} src={profilePicture.url} />
+    <h1>{displayName}</h1>
+    <p>{bio}</p>
+    <p>{contactEmail}</p>
+    <p>{contactNumber}</p>
+  </div>
+);
+
+export default ProfileView;
