ec37fba259d42eb486baa6e6773824543a055ace
James <jameslimbj@gmail.com>
2021-06-12 14:53:01 +0800

    :hammer: REFACTOR (api): add separating newline

--- src/api/fakeUserAPI.ts
+++ src/api/fakeUserAPI.ts
@@ -7,6 +7,7 @@ const userAPI = {
     await new Promise(resolve => setTimeout(resolve, 1000));
     return server.users.find(({ uuid }) => uuid === _uuid);
   },
+
   fetchWithUsername: async (
     _username: string
   ): Promise<ProfileModel | undefined> => {
@@ -14,11 +15,13 @@ const userAPI = {
     await new Promise(resolve => setTimeout(resolve, 1000));
     return server.users.find(({ username }) => username === _username);
   },
+
   fetchUUID: async (_username: string): Promise<string | undefined> => {
     // TODO(IRWEN): replace with actual API calls
     await new Promise(resolve => setTimeout(resolve, 1000));
     return server.users.find(({ username }) => username === _username)?.uuid;
   },
+
   fetchUsername: async (_uuid: string): Promise<string | undefined> => {
     // TODO(IRWEN): replace with actual API calls
     await new Promise(resolve => setTimeout(resolve, 1000));
