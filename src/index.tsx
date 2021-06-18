45ad4ece47e2b8a62882aacf828419ec2a067bf3
James Lim <jameslimbj@gmail.com>
2021-06-18 10:49:19 +0800

    :hammer: REFACTOR (redux): temporarily remove provider (#61)

--- src/index.tsx
+++ src/index.tsx
@@ -5,11 +5,11 @@ import { Provider } from "react-redux";
 import "./index.css";
 
 import App from "./App";
-import store from "./store";
+// import store from "./store";
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  // <Provider store={store}>
+  <App />,
+  // </Provider>,
   document.getElementById("root")
 );
