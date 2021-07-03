14f51174291e11befb644d13fff8e90d4bd39988
James <jameslimbj@gmail.com>
2021-07-03 18:48:11 +0800

    :sparkles: FEAT (component): add link to tags to view filtered projects

--- src/components/Tag/index.tsx
+++ src/components/Tag/index.tsx
@@ -1,14 +1,21 @@
 import React, { HTMLAttributes } from "react";
+import { Link } from "react-router-dom";
 
-const Tag: React.FC<HTMLAttributes<HTMLButtonElement>> = ({
-  className,
-  children,
+import TagModel from "models/Tag";
+
+type TagProps = TagModel & {
+  currentFilter: number;
+} & HTMLAttributes<HTMLElement>;
+
+const Tag: React.FC<TagProps> = ({
+  flag,
+  name,
+  username,
+  currentFilter,
   ...rest
-}: HTMLAttributes<HTMLButtonElement>) => (
-  <li className={`tag ${className || ""}`}>
-    <button type="button" {...rest}>
-      {children}
-    </button>
+}: TagProps) => (
+  <li {...rest}>
+    <Link to={`/${username}?tags=${flag | currentFilter}`}>{name}</Link>
   </li>
 );
 
--- src/components/TagList/index.tsx
+++ src/components/TagList/index.tsx
@@ -1,17 +1,33 @@
 import React, { HTMLAttributes } from "react";
 
 import Tag from "components/Tag";
+import TagModel from "models/Tag";
 
 interface TagListProps extends HTMLAttributes<HTMLElement> {
-  tags: string[];
+  tags: TagModel[];
+  currentFilter?: number;
 }
 
-const TagList: React.FC<TagListProps> = ({ tags, ...rest }: TagListProps) => (
+const TagList: React.FC<TagListProps> = ({
+  tags,
+  currentFilter,
+  ...rest
+}: TagListProps) => (
   <ul {...rest}>
     {tags.map(tag => (
-      <Tag key={tag}>{tag}</Tag>
+      <Tag
+        flag={tag.flag}
+        name={tag.name}
+        username={tag.username}
+        currentFilter={currentFilter || 0}
+        key={tag.name}
+      />
     ))}
   </ul>
 );
+
+TagList.defaultProps = {
+  currentFilter: 0,
+};
 
 export default TagList;
