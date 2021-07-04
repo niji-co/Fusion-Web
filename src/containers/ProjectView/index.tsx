c2992f9a24a434b2ba7c2c3d3af255af5ea379c4
James <jameslimbj@gmail.com>
2021-07-05 02:34:42 +0800

    chore (imports): remove unused imports

--- src/containers/ProjectView/index.tsx
+++ src/containers/ProjectView/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 
 import ProjectView from "components/ProjectView";
 import useProfileWithUsername from "hooks/useProfileWithUsername";
