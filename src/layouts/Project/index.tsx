289b7fdb13fc7f2c9775876344f7098d69d1b809
James <jameslimbj@gmail.com>
2021-07-06 10:02:33 +0800

    :hammer: REFACTOR (layouts): remove html attributes from props

--- src/layouts/Project/index.tsx
+++ src/layouts/Project/index.tsx
@@ -1,15 +1,13 @@
-import React, { HTMLAttributes } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 
 import ProjectViewContainer from "containers/ProjectView";
 import { ProjectQueryModel } from "models/Project";
 
-type ProjectLayoutProps = ProjectQueryModel & HTMLAttributes<HTMLElement>;
-
-const ProjectLayout: React.FC<ProjectLayoutProps> = ({
+const ProjectLayout: React.FC<ProjectQueryModel> = ({
   authorUsername,
   projectTitle,
-}: ProjectLayoutProps) => (
+}: ProjectQueryModel) => (
   <>
     <Link to="/">Home</Link>
     <ProjectViewContainer
