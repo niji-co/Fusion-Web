2ae3a0a75e388bdf653590e1cc8717b70c0b99bb
Sophie <sophiewongsp@gmail.com>
2021-07-06 18:01:11 +0800

    🔨 REFACTOR (components/ExhibitionRole) : replace use of React FS

--- src/components/ExhibitionRole/index.tsx
+++ src/components/ExhibitionRole/index.tsx
@@ -1,10 +1,13 @@
-import React, { HTMLAttributes } from "react";
+import React, { ReactElement, HTMLAttributes } from "react";
 
 import RoleModel from "models/ExhibitionRole";
 
 type RoleProps = RoleModel & HTMLAttributes<HTMLElement>;
 
-const Role: React.FC<RoleProps> = ({ role }: RoleProps) => (
+const Role = ({
+  role,
+  className,
+}: RoleProps): ReactElement => (
   <div>
     <h3>{role}</h3>
   </div>
