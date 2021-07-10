11dfd3e694dd9b040eae440dec9f7ff5bd93b3df
James <jameslimbj@gmail.com>
2021-07-10 17:18:05 +0800

    :art: FORMAT (models): prefix private files with _

--- src/models/Project/index.ts
+++ src/models/Project/index.ts
@@ -1,5 +1,5 @@
-import ProjectModel from "./ProjectModel";
-import ProjectQueryModel from "./ProjectQueryModel";
+import ProjectModel from "./_ProjectModel";
+import ProjectQueryModel from "./_ProjectQueryModel";
 
 export default ProjectModel;
 export { ProjectQueryModel };
