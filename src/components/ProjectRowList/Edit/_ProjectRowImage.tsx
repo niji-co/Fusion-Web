bde2f66d9bd3c08acb707b63c5319ec14bf878d7
James <jameslimbj@gmail.com>
2022-07-05 15:41:33 +0800

    :sparkles: FEAT (components): add new image row basic features

--- src/components/ProjectRowList/Edit/_ProjectRowImage.tsx
+++ src/components/ProjectRowList/Edit/_ProjectRowImage.tsx
@@ -1,19 +1,33 @@
-import React, { ReactElement, HTMLAttributes } from "react";
+import React, { ReactElement, HTMLAttributes, ChangeEvent } from "react";
 
-import { ProjectRowAssetModel } from "models/ProjectRow";
+import ProjectRowModel, { ProjectRowAssetModel } from "models/ProjectRow";
 
 interface EditProjectRowImageProps extends HTMLAttributes<HTMLElement> {
   model: ProjectRowAssetModel;
+  updateRow: (value: ProjectRowModel) => void;
 }
 
 const EditProjectRowImage = ({
   model,
+  updateRow,
   className,
   ...rest
-}: EditProjectRowImageProps): ReactElement => (
-  <div className={`project-row-image ${className || ""}`} {...rest}>
-    <img alt={model.asset.name} src={model.asset.url} />
-  </div>
-);
+}: EditProjectRowImageProps): ReactElement => {
+  const handleTextChange = (event: ChangeEvent<HTMLInputElement>): void => {
+    const newAsset = { asset: { ...model.asset, url: event.target.value } };
+    updateRow({ ...model, ...newAsset });
+  };
+
+  return (
+    <div className={`project-row-image ${className || ""}`} {...rest}>
+      <input
+        placeholder="Image URL"
+        value={model.asset.url}
+        onChange={handleTextChange}
+      />
+      <img alt={model.asset.name} src={model.asset.url} />
+    </div>
+  );
+};
 
 export default EditProjectRowImage;
